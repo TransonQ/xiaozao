31cc66235d5cb76ef82f9011ec2a7c16092f380d
quanscheng <quanscheng@163.com>
2022-12-04 23:01:22 +0800

    了解钩子

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const hasLearned = true
-
-  // if (hasLearned) {
-  //   return <h1>张无忌学会了乾坤大挪移一人爆锤六大门派</h1>
-  // } else {
-  //   return <h1>张无忌偶遇山猴获九阳神功逼出寒毒</h1>
-  // }
+  // const hasLearned = true
+  const [hasLearned, setHasLearned] = useState(false)
+  const handleChange = () => setHasLearned(!hasLearned)
 
   let JSXMarkup = null
 
@@ -17,7 +14,12 @@ function App() {
     JSXMarkup = <h1>张无忌偶遇山猴获九阳神功逼出寒毒</h1>
   }
 
-  return <>{JSXMarkup}</>
+  return (
+    <>
+      <button onClick={handleChange}>切换武功状态</button>
+      {JSXMarkup}
+    </>
+  )
 }
 
 export default App
