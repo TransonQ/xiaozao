d0f63cf29b4e855b2b060c49c992eeda6a12310d
quanscheng <quanscheng@163.com>
2022-12-11 15:29:02 +0800

    2-1 props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(1)
+
   return (
-    <div >
-      这是 app
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        {`当前值${count > 10 ? '大于 10' : '在 1 - 10 以内'}: ${count}`}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
