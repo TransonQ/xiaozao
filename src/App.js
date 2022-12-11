8d887909a83831aff6dbec6da084b88b55006af8
quanscheng <quanscheng@163.com>
2022-12-11 22:51:51 +0800

    增删的 ToDolist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { List } from './List'
 
 function App() {
+  // 数据驱动 UI , 这个状态就是列表渲染的数据
+  const [todoList, setTodoList] = useState([])
+  // input 需要受控
+  const [input, setInput] = useState('')
+  // 点击 add 的回调函数
+  const handleAdd = () => {
+    setTodoList((prevList) => [input, ...prevList])
+    setInput('')
+  }
+
+  useEffect(() => {
+    console.log('todoList: ', todoList)
+  }, [todoList])
+
   return (
     <div className='contain'>
       <h3>todo list</h3>
       <div>
-        todo: <input /> <button>新增</button>
+        input
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
       </div>
-      <List />
+      <List
+        list={todoList}
+        setList={setTodoList}
+      />
     </div>
   )
 }
--- src/List.jsx
+++ src/List.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 
 import './list.css'
 
-export const List = () => {
+export const List = ({ list, setList }) => {
+  // // 点击 x 的回调函数
+  // const handleDelete = (i) => () => {
+  //   setList((prevList) => {
+  //     return prevList.filter((_, index) => index !== i)
+  //   })
+  // }
+
   return (
     <ul className='list'>
-      <li>
-        todo item <div className='delete'>x</div>
-      </li>
+      {list.map((s, i) => {
+        return (
+          <li key={i}>
+            {s}
+            <div
+              className='delete'
+              // onClick={handleDelete(i)}
+              onClick={() =>
+                setList((list) => list.filter((_, index) => index !== i))
+              }
+            >
+              x
+            </div>
+          </li>
+        )
+      })}
     </ul>
   )
 }
